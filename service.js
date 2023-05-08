808be56a7c19d20b678efa114afbd4ce6a836821
palani <palanibaba2002@gmail.com>
2023-05-08 11:52:48 +0530

    Middleware Used in Service

--- service.js
+++ service.js
@@ -4,38 +4,34 @@ const serviceRouter = express.Router()
 
 const ListOfServices = [
     {
-        "id":1,
+        // "id":1,
         "Name":"Application Development"
     },
     {
-        "id":2,
+        // "id":2,
         "Name":"Website Creation"
     },
     {
-        "id":3,
+        // "id":3,
         "Name":"Cloud Services"
     },
     {
-        "id":4,
+        // "id":4,
         "Name":"Saas Services"
     }
 ]
-serviceRouter.get('/:id',(req,res)=>{
-    // res.send("Service Page")
-    const UserId = Number(req.params.id)
-    const isAvail = ListOfServices.find((obj1)=>(
-        obj1.id == UserId
-    ))
-    if(!isAvail)
-    {
-        console.log("Service Not Found")
-        res.sendStatus(500)
-    }
-    else{
-        console.log(isAvail.Name)
-        res.json(isAvail.Name)
-    }
+serviceRouter.get('/',(req,res)=>{
+    res.send("Service Page")
+})
 
+serviceRouter.get('/:id',(req,res)=>{
+    // res.send(req.user.Name)
+    res.json(req.user.Name)
+})
+
+serviceRouter.param('id',(req,res,next,id)=>{
+    req.user = ListOfServices[id-1]
+    next()
 })
 
 module.exports = serviceRouter
